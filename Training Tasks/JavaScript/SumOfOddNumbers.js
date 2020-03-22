27f9f3887ab56700094587983cf46dfcab585f02
Lyadov <lyadov.vicheslav.sergeevich@gmail.com>
2020-03-22 21:31:49 +0300

    origin

---
+++ Training Tasks/JavaScript/SumOfOddNumbers.js
@@ -0,0 +1,34 @@
+function rowSumOddNumbers(n) {
+    var fseek = 0, index = 1, result = 0;
+  
+    for(var i = 1; i<n; i++)
+    {
+        fseek+=i;
+    }
+
+    while(fseek != 0)
+    {
+        if(index % 2 == 1)
+        {
+            fseek--;
+        }
+        index++;
+    }
+
+    do{
+        if(index%2==1)
+        {
+            result += index;
+            n--;
+        }
+        index++;
+    }while(n!=0)
+  
+  return result;
+}
+
+
+// Short solutions
+ function rowSumOddNumbersShort(n) {
+   return n*n*n;
+}
