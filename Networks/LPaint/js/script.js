9d504b36a8506ddd5832ab10b0cda6744037b5ff
Lyadov <lyadov.vicheslav.sergeevich@gmail.com>
2020-06-29 17:20:55 +0300

    LPaint v.0.4

--- Networks/LPaint/js/script.js
+++ Networks/LPaint/js/script.js
@@ -5,6 +5,9 @@ var red = 0,
 
 var lineWeigth = 5;
 
+var figure = 1;
+
+
 window.onload = function(){
 
     var canvas = document.getElementById("canvas"), 
@@ -23,33 +26,163 @@ window.onload = function(){
         context.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
         mouse.x = e.pageX - this.offsetLeft;
         mouse.y = e.pageY - this.offsetTop;
-        draw = true;
-        context.beginPath();
-        context.moveTo(mouse.x, mouse.y);
+        if(figure == 1) // curve
+        {
+
+            draw = true;
+            context.beginPath();
+            context.moveTo(mouse.x, mouse.y);
+        }   
+        else if(figure == 2)
+        {
+           
+        } 
+        else if(figure == 3) // circle
+        {
+            
+        }   
     });
 
     // Event MouseMove  
     canvas.addEventListener("mousemove", function(e){
-    if(draw==true){
-        
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-        context.lineTo(mouse.x, mouse.y);
-        context.stroke();
-    }
+        if(figure == 1) // curve
+        {
+            if(draw==true)
+            {
+                mouse.x = e.pageX - this.offsetLeft;
+                mouse.y = e.pageY - this.offsetTop;
+                context.lineTo(mouse.x, mouse.y);
+                context.stroke();      
+            }
+        }
     });
+
 
     // Event MouseUp  
-    canvas.addEventListener("mouseup", function(e){   
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-        context.lineTo(mouse.x, mouse.y);
-        context.stroke();
-        context.closePath();
-        draw = false;
-    });
+    canvas.addEventListener("mouseup", function(e){ 
+        if(figure == 1) // curve
+        {  
+            mouse.x = e.pageX - this.offsetLeft;
+            mouse.y = e.pageY - this.offsetTop;
+            context.lineTo(mouse.x, mouse.y);
+            context.stroke();
+            context.closePath();
+            draw = false;
+        
+        }
+        else if(figure == 2)
+        {
+            context.beginPath();
+            context.moveTo(mouse.x, mouse.y);
+            context.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            context.stroke();
+            context.closePath();
+        } 
+        else if(figure == 3) // circle
+        {
+            context.beginPath();
+            context.arc((e.pageX - this.offsetLeft), (e.pageY - this.offsetTop), (e.pageX - this.offsetLeft)-mouse.x, 0, (Math.PI / 180) * 360);
+            context.stroke();
+            context.closePath();
+        } 
+        else if(figure == 4) // rectangle
+        {
+            context.beginPath();
+            context.rect(mouse.x, mouse.y, (e.pageX - this.offsetLeft)-mouse.x, (e.pageY - this.offsetTop)-mouse.y);
+            context.stroke();
+            context.closePath();
+        }
+        else if(figure == 5) // triangle
+        {
+            context.beginPath();
 
-    
+            context.moveTo(mouse.x, mouse.y);
+            context.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+
+            context.moveTo(mouse.x, mouse.y);
+            context.lineTo(mouse.x, e.pageY - this.offsetTop);
+
+            context.moveTo(mouse.x, e.pageY - this.offsetTop);
+            context.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            
+            context.stroke();
+            context.closePath();
+        }
+        else if(figure == 6) // triangle #2
+        {
+            context.beginPath();
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, mouse.y);
+            context.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, mouse.y);
+            context.lineTo(mouse.x, e.pageY - this.offsetTop);
+
+            context.moveTo(mouse.x, e.pageY - this.offsetTop);
+            context.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            
+            context.stroke();
+            context.closePath();
+        }
+        else if(figure == 7) // triangle #2
+        {
+            context.beginPath();
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, mouse.y);
+            context.lineTo((e.pageX - this.offsetLeft), (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, mouse.y);
+            context.lineTo(mouse.x, (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, e.pageY - this.offsetTop);
+            context.lineTo((e.pageX - this.offsetLeft), (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, e.pageY - this.offsetTop);
+            context.lineTo(mouse.x, (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+
+            context.stroke();
+            context.closePath();
+        }
+        else if(figure == 8) // up-arrow
+        {
+            context.beginPath();
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, mouse.y);
+            context.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+
+            context.moveTo(mouse.x +((e.pageX - this.offsetLeft)-mouse.x)/2, mouse.y);
+            context.lineTo(mouse.x, e.pageY - this.offsetTop);
+
+            context.stroke();
+            context.closePath();
+        }
+        else if(figure == 9) // next arrow
+        {
+            context.beginPath();
+
+            context.moveTo(mouse.x, mouse.y);
+            context.lineTo((e.pageX - this.offsetLeft), (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+
+            context.lineTo((e.pageX - this.offsetLeft), (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+            context.lineTo(mouse.x, (e.pageY - this.offsetTop));
+
+            context.stroke();
+            context.closePath();
+        }
+        else if(figure == 10) // triangle #2
+        {
+            context.beginPath();
+
+            context.moveTo(e.pageX - this.offsetLeft, mouse.y);
+            context.lineTo(mouse.x, (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+
+            context.moveTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            context.lineTo(mouse.x, (e.pageY - this.offsetTop)-((e.pageY - this.offsetTop)-mouse.y)/2);
+
+            context.stroke();
+            context.closePath();
+        }
+        });
 }
 
 // Set current color
@@ -74,3 +207,7 @@ function SetCustomWeigth(){
         lineWeigth = document.getElementById("thickness_size_custom").value;
 }
 
+// Set Custom Pen
+function SetFigure(number){
+    figure = number;
+}
