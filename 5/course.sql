81888cc41fd6660c19a670b0050729bb175bc2ea
Вячеслав <lyadov.vicheslav.sergeevich@gmail.com>
2020-11-25 13:20:45 +0300

    del folder 5

--- 5/course.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO course(name, teacher)
-VALUES ('Компьютерная грамотность', 22);
-
-INSERT INTO course(name, teacher)
-VALUES ('Компьютерная графика', 23);
-
-INSERT INTO course(name, teacher)
-VALUES ('Основы веб-разработки', 24);
-
